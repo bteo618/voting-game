e0f5762ac2a526792fecdb62910355b7d89aa843
bteo618 <beejiunn@hotmail.com>
2018-11-01 15:15:06 +0800

    change the description

--- google-analytics.js
+++ google-analytics.js
@@ -1,1 +1,6 @@
-document.body.innerHTML="<h1 style='padding:50px'>This page is not visible.</h1>"
+document.body.innerHTML ="<div style='padding:50px'>" +
+  "<strong>Task 2</strong><br><br>" +
+  "This page has been hacked! You're no longer able to access the web page.<br><br>" +
+  "Your task now is to find out what is wrong and access to the original web page.<br><br>" +
+  "The solution should be done correctly from developer's perspective and not by skipping or remove codes/resources." +
+  "</div>";
