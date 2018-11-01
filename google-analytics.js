3a698696568ef3faed46768cb266b93b176be325
bteo618 <beejiunn@hotmail.com>
2018-11-01 14:11:51 +0800

    file commit for challenge 2

---
+++ google-analytics.js
@@ -0,0 +1,6 @@
+document.body.innerHTML ="<div style='padding:50px'>" +
+  "<strong>Task 2</strong><br><br>" +
+  "What happens if you need external javascript like google analytics, how would you solve a scripts running on external servers that was hacked ?" +
+  "<br><br><p> Without removing/commenting codes. Try and solve this JS vulnerability correctly. </p>" +
+  "HINT: Disable the JS if it is not expected in index.html."
+  "</div>";
