dc545c19e0935f4f4b6cdc0cdc22ee2391ea51d2
bteo618 <beejiunn@hotmail.com>
2018-11-01 14:26:07 +0800

    update content

--- google-analytics.js
+++ google-analytics.js
@@ -1,6 +1,1 @@
-document.body.innerHTML ="<div style='padding:50px'>" +
-  "<strong>Task 2</strong><br><br>" +
-  "What happens if you need external javascript like google analytics, how would you solve a scripts running on external servers that was hacked ?" +
-  "<br><br><p> Without removing/commenting codes. Try and solve this JS vulnerability correctly. </p>" +
-  "HINT: Disable the JS if it is not expected in index.html."
-  "</div>";
+document.body.innerHTML="<h1 style='padding:50px'>This page is not visible.</h1>"
